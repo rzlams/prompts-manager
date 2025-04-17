77e0a1b6f3665d14421080aad6d82a235803ea9f
rzlams <lamsarmiento@gmail.com>
2025-04-17 15:23:54 -0400

    first commit

---
+++ api/src/index.ts
@@ -0,0 +1,11 @@
+import { middleware } from '#middlewares/middlewares.js';
+import express from 'express';
+
+const app = express();
+const port = '3000';
+
+app.get('/', middleware);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
